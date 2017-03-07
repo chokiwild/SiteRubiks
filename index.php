a97d9b32bc2076cbea9b188b011e003712349479
chokiwild <lou.dvl@eduge.ch>
2017-03-07 08:18:21 +0100

    firstCommit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Accueil</title>
+        <meta charset="utf8" lang="fr">
+        <link href="mainStyle.css" rel="stylesheet">
+    </head>
+    <body>
+        <div id='container'>
+        <header>
+            <div id='logo'>
+            <img src='img/titre1.png'/>
+            </div>
+        </header>
+        <section>
+            <nav>
+                <ul>
+                    <li>Accueil</li>
+                    <li>Résolution</li>
+                    <li>Forum</li>
+                    <li>Nous contacter</li>
+                </ul>
+            </nav>
+            <article>
+                <div class="bandeTitre"><h2>Banderolle test</h2></div>
+            </article>
+        </section>
+        <footer>
+            
+        </footer>
+        </div>
+    </body>
+</html>
