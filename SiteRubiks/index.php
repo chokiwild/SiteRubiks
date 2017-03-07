c7414b642b5d5a43f2af4f2686a915311f73aacc
chokiwild <lou.dvl@eduge.ch>
2017-03-07 09:31:06 +0100

    modif2

---
+++ SiteRubiks/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Accueil</title>
+        <meta charset="utf8" lang="fr">
+        <link href="mainStyle.css" rel="stylesheet">
+    </head>
+    <body>
+        <div id='container'>
+        <header>
+            <div id='logo'>
+            <img src='img/titre1.png'/>
+            </div>
+        </header>
+        <section>
+            <nav>
+                <ul>
+                    <li class="menu"><a href="index.php" id="active">Accueil</a></li>
+                    <li class="menu"><a href="resolution.php">Résolution</a></li>
+                    <li class="menu"><a href="forum.php">Forum</a></li>
+                    <li class="menu"><a href="contacter.php">Nous contacter</a></li>
+                </ul>
+            </nav>
+            <article>
+                <div class="bandeTitre"><h2>Banderolle test</h2></div>
+            </article>
+        </section>
+        <footer>
+            
+        </footer>
+        </div>
+    </body>
+</html>
